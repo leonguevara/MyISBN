6ef7bb77e1b2b2af2a6256af633500189a6fd660
leonguevara <leon.guevara@outlook.com>
2015-12-27 19:35:24 -0600

    uso y análisis de JSON

--- MyISBN/ViewController.swift
+++ MyISBN/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var isbn: UITextField!
     @IBOutlet weak var respuesta: UITextView!
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var autor: UILabel!
+    @IBOutlet weak var myURL: UILabel!
     
     
     override func viewDidLoad() {
@@ -31,13 +34,27 @@ class ViewController: UIViewController {
 
     @IBAction func buscarISBN() {
         let myISBN:String? = isbn.text!
-        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(myISBN!)"
+        let mISBN = myISBN!
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
         /*print(urls)*/
-        let url = NSURL(string: urls)
+        let url = NSURL(string: urls + myISBN!)
         let datos:NSData? = NSData(contentsOfURL: url!)
         let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
         /*print(texto)*/
         respuesta.text = texto! as String
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+            let dict1 = json as! NSDictionary
+            let dict2 = dict1["ISBN:" + mISBN] as! NSDictionary
+            self.titulo.text = dict2["title"] as! NSString as String
+            self.autor.text = dict2["by_statement"] as! NSString as String
+            let dict3 = dict2["cover"] as! NSDictionary
+            self.myURL.text = dict3["small"] as!NSString as String
+        }
+        catch _ {
+            
+        }
     }
+    
 }
 
