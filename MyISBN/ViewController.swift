dca106ac60e688691409a34ee0ec42b032d6ed95
leonguevara <leon.guevara@outlook.com>
2016-02-01 17:57:34 -0600

    Se resolvió el manejo de autores por arreglo.  Se agregó alerta para cuando no se tiene conexión.  Se agregó código para cuando la imagen no existe.

--- MyISBN/ViewController.swift
+++ MyISBN/ViewController.swift
@@ -41,21 +41,34 @@ class ViewController: UIViewController {
         
         /*print(texto)*/
         do {
+            var autoresLibro : String = ""
             let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
             let dict1 = json as! NSDictionary
             let dict2 = dict1["ISBN:" + mISBN] as! NSDictionary
             self.titulo.text = dict2["title"] as! NSString as String
-            self.autor.text = dict2["by_statement"] as! NSString as String
+            let autores = dict2["rauthors"] as! NSArray
+            for autorLibro in autores {
+                let autorTemp = (autorLibro as! NSDictionary)["name"] as! String
+                autoresLibro += autorTemp
+                autoresLibro += "; "
+            }
+            if (autoresLibro != "") {
+                self.autor.text = autoresLibro
+            } else {
+                self.autor.text = "Sin autor registrado"
+            }
             let dict3 = dict2["cover"] as! NSDictionary
             self.myURL.text = dict3["small"] as!NSString as String
             let urlImage = NSURL(string: self.myURL.text!)
             let datos2 = NSData(contentsOfURL: urlImage!)
             if datos2 != nil {
-                myImage.image = UIImage(data: datos2!);
+                myImage.image = UIImage(data: datos2!)
             }
         }
         catch _ {
+            let alert = UIAlertController(title: "Oops!", message: "Verifica que tengas conexión", preferredStyle: .Alert)
             
+            self.presentViewController(alert, animated: true, completion: nil)
         }
     }
     
